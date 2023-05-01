40aa96e8408ae80087e75fd9315db860a02565ec
Lengyel Bálint <112940758+lengyelb1@users.noreply.github.com>
2023-05-01 21:39:13 +0200

    Add files via upload

---
+++ selenium_test.py
@@ -0,0 +1,40 @@
+from distutils.spawn import find_executable
+from lib2to3.pgen2 import driver
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+bongeszo = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+
+#Oldal megnyitás
+bongeszo.get('https://fashion-shop2.netlify.app/')
+bongeszo.maximize_window()
+time.sleep(3)
+
+
+btn = bongeszo.find_element(By.XPATH, '/html/body/div/div[1]/nav/ul/a[1]/li')
+btn.click()
+time.sleep(1)
+bongeszo.save_screenshot('printscreen1.png')
+
+btn = bongeszo.find_element(By.XPATH, '/html/body/div/div[1]/nav/ul/a[2]/li')
+btn.click()
+time.sleep(1)
+bongeszo.save_screenshot('printscreen2.png')
+
+btn = bongeszo.find_element(By.XPATH, '/html/body/div/div[1]/nav/ul/a[3]/li')
+btn.click()
+time.sleep(1)
+bongeszo.save_screenshot('printscreen3.png')
+
+btn = bongeszo.find_element(By.XPATH, '/html/body/div/div[1]/nav/ul/a[4]/li')
+btn.click()
+time.sleep(1)
+bongeszo.save_screenshot('printscreen4.png')
+
+btn = bongeszo.find_element(By.XPATH, '/html/body/div/div[1]/nav/ul/a[5]/li')
+btn.click()
+time.sleep(1)
+bongeszo.save_screenshot('printscreen5.png')
